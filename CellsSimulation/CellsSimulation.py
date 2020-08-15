944b0e86b0b07086a9bff2ddc3cb3d0a9b19d79f
dor dolev <ddoolev@gmail.com>
2020-08-16 00:34:02 +0300

    some fixes

--- CellsSimulation/CellsSimulation.py
+++ CellsSimulation/CellsSimulation.py
@@ -2,16 +2,24 @@ import numpy as np
 import Simulation as sim
 from cell_creator.DefaultCell import DefaultCell as DCell
 from scipy.sparse import spdiags
+from LaplaceOperator import LaplaceOperator
 
 #simulation = sim.Simulation()
 #round_cell = DCell(center = [0,0],r = 50, r_split = 100)
 #simulation.addCell(round_cell)
 #simulation.simulationStart()
 
-data = np.array([[1, 0, 0, 0], [0, 2, 0, 0], [0, 0, 3, 4]])
+data_array = np.array([[1,2,3,4],
+                       [5,6,7,8],
+                       [9,10,11,12],
+                       [13,14,15,16]])
 
-diags = np.array([-2,-1,0])
+delta_vector = np.array([1,1,1,1])
 
-print(spdiags(data, diags, 4, 4))
+laplas = LaplaceOperator(delta_vector, delta_vector)
+results = laplas.laplacianOperation(data_array)
+
+print(results)
+
 
 
--- CellsSimulation/LaplaceOperator.py
+++ CellsSimulation/LaplaceOperator.py
@@ -3,66 +3,77 @@ import scipy
 
 class LaplaceOperator:
 
-    def __init__(self, data_matrix, delta_x, delta_y):
+    def __init__(self, delta_x, delta_y):
         self.__operators_matrix_diagonals = \
-            __createLaplacOperatorsMatrix(self, data_matrix, delta_x, delta_y)
+            self.__createLaplacOperatorsMatrix(delta_x, delta_y)
 
     def laplacianOperation(self, data_matrix):
+        data_matrix = data_matrix.flatten
         return self.__operators_matrix_diagonals @ data_matrix
 
     # create Laplac operator's matrix, to calculate the laplacian for every point faster
-    @classmethod
-    def __createLaplacOperatorsMatrix(self, data_matrix, delta_x, delta_y):
-        num_of_points = len(data_matrix)*len(data_matrix[0])
+    
+    def __createLaplacOperatorsMatrix(self, delta_x, delta_y):
+        grid_length_x = len(delta_x) + 1
+        grid_length_y = len(delta_y) + 1
+        num_of_points_in_matrix = grid_length_x*grid_length_y
         operators_matrix_diagonals = [[],[],[],[],[]]
 
         boundary_vector = [[0],[0],[0],[0],[0]]
-        boundry_values_block = self.__createBoundryValueBlock()
+        boundry_values_block = self.__createBoundryValueBlock(grid_length_x)
 
         operators_matrix_diagonals = \
             np.concatenate((operators_matrix_diagonals, boundry_values_block), 1)
         
         # Iterate on the matrix blocks
-        for i in range(len(data_matrix), num_of_points - len(data_matrix), len(data_matrix)):
-            block = self.__createLaplacOperatorsMatrixBlock(delta_x, delta_y)
+        for i in range(grid_length_x, num_of_points_in_matrix - grid_length_x, grid_length_x):
+            block = self.__createLaplacOperatorsMatrixBlock(delta_x, delta_y, i)
             operators_matrix_diagonals = \
                 np.concatenate((operators_matrix_diagonals, block), 1)
 
         operators_matrix_diagonals = \
             np.concatenate((operators_matrix_diagonals, boundry_values_block), 1)
 
-    @classmethod
-    def __createBoundryValueBlock(self):
+        return operators_matrix_diagonals
+
+
+    def __createBoundryValueBlock(self, grid_length_x):
         block = [[],[],[],[],[]]
         boundary_vector = [[0],[0],[0],[0],[0]]
-        for i in range(len(data_matrix)):
+        for i in range(grid_length_x):
             block = np.concatenate((block, boundary_vector), 1)
         return block
 
-    @classmethod
-    def __createLaplacOperatorsMatrixVector(self, delta_x, delta_y):
+    
+    def __createLaplacOperatorsMatrixVector(self, delta_x, delta_y, i):
         # create the coeficients. 
         # P=plus, M=minus
-        coe_i_jM1 = 2 / (delta_y[i-1] * (delta_y[i] + delta_y[i-1]))
-        coe_iM1_j = 2 / (delta_x[i-1] * (delta_x[i] + delta_x[i-1]))
-        coe_i_j = 2 / (delta_x[i] * delta_x[i-1]) + 2 / (delta_y[i] * delta_y[i-1])
-        coe_iP1_j = 2 / (delta_x[i] * (delta_x[i] + delta_x[i-1]))
-        coe_i_jP1 = 2 / (delta_y[i] * (delta_y[i] + delta_y[i-1]))
+        grid_length_x = len(delta_x) + 1
+        x_index = i//grid_length_x
+        y_index = i%grid_length_x
+
+        coe_i_jM1 = 2 / (delta_y[y_index-1] * (delta_y[y_index] + delta_y[y_index-1]))
+        coe_iM1_j = 2 / (delta_x[x_index-1] * (delta_x[x_index] + delta_x[x_index-1]))
+        coe_i_j = 2 / (delta_x[x_index] * delta_x[x_index-1]) + 2 / (delta_y[y_index] * delta_y[y_index-1])
+        coe_iP1_j = 2 / (delta_x[x_index] * (delta_x[x_index] + delta_x[x_index-1]))
+        coe_i_jP1 = 2 / (delta_y[y_index] * (delta_y[y_index] + delta_y[y_index-1]))
 
         coe_vector = [[coe_i_jM1],[coe_iM1_j],[coe_i_j],[coe_iP1_j],[coe_i_jP1]]
 
         return coe_vector
 
-    @classmethod
-    def __createLaplacOperatorsMatrixBlock(self, delta_x, deltay):
+    
+    def __createLaplacOperatorsMatrixBlock(self, delta_x, delta_y, i):
         block = [[],[],[],[],[]]
+        boundary_vector = [[0],[0],[0],[0],[0]]
+        grid_length_x = len(delta_x) + 1
         # boundary value
         block = np.concatenate((block, boundary_vector), 1)
 
         # Create a block in the matrix
-        for j in range(i+1, i+len(data_matrix)-1):
-            coe_vector = self.__createLaplacOperatorsMatrixVector(delta_x,delta_y)
-            block = np.concatenate((block, coe_vector), 1)
+        for j in range(i+1, i+grid_length_x-1):
+            coeficient_vector = self.__createLaplacOperatorsMatrixVector(delta_x, delta_y, j)
+            block = np.concatenate((block, coeficient_vector), 1)
 
         # boundary value
         block = np.concatenate((block, boundary_vector), 1)
