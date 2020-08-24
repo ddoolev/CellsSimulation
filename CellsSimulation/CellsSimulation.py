326b1552f92fb059f0cf36a349489928cd025a72
dor dolev <ddoolev@gmail.com>
2020-08-24 14:40:58 +0300

    create navier stokes folder

--- CellsSimulation/CellsSimulation.py
+++ CellsSimulation/CellsSimulation.py
@@ -1,25 +1,18 @@
 import numpy as np
 import Simulation as sim
 from cell_creator.DefaultCell import DefaultCell as DCell
-from scipy.sparse import spdiags
-from LaplaceOperator import LaplaceOperator
+from navier_stokes_equations.MathOperations import MathOperations
 
 #simulation = sim.Simulation()
 #round_cell = DCell(center = [0,0],r = 50, r_split = 100)
 #simulation.addCell(round_cell)
 #simulation.simulationStart()
 
+array_size = 5
+p_array = np.random.randint(10, size = (array_size,array_size))
+delta_vector = np.arange(1,array_size)
 
-array_size = 1000
-data_array = np.random.randint(10, size=(array_size, array_size))
-#print(data_array)
-
-delta_vector = np.full((array_size-1), 1)
-
-laplas = LaplaceOperator(delta_vector, delta_vector)
-results = laplas.laplacianOperation(data_array)
-
-print(results)
-
-
+print(p_array)
+print(delta_vector)
+print(MathOperations.gradientY(p_array,delta_vector))
 
---
+++ CellsSimulation/navier_stokes_equations/NavierStokesEquations.py
@@ -0,0 +1,9 @@
+import numpy as np
+import scipy 
+
+class NavierStokesEquations:
+
+    def __init__(self, p_matrix, v_matrix, u_matrix, delta_x, delta_y):
+        
+
+    def __gradient_x(self, p_matrix, delta_x):
