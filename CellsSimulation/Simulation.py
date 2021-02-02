85d90cd0d52f5da89113af9c9af1e1ec3981fe77
dor dolev <ddoolev@gmail.com>
2021-02-02 11:58:25 +0200

    redo Simulation class

--- CellsSimulation/Simulation.py
+++ CellsSimulation/Simulation.py
@@ -1,66 +1,41 @@
+from matplotlib import pyplot as plt
+import General_enums as E
 import numpy as np
-import matplotlib.pylab as plb
-import matplotlib.pyplot as plt
-import Constants as C
-import Cell
+from NavierStokesEquations import NavierStokesEquations
+from matplotlib import animation
 
 
 class Simulation:
 
-    def __init__(self, size=1000, cells=np.array([]), time=20):
-        self.size = size
-        self.cells = cells
-        self.time = time
-        self.fig = plb.figure()
-        self.axes = plb.axes(xlim=[-self.size, self.size], ylim=[-self.size, self.size])
-        self.__boundaries = [plb.plot([], [])[0] for _ in range(len(self.cells))]
+    def __init__(self, fields, grid, boundaries, delta_t=0.01, time_steps=100,
+                 boundary_conditions_type=E.BoundaryConditionsType.neumann,
+                 information=E.Information.none,
+                 cells=np.array([])):
+        self.__domain = NavierStokesEquations(fields, grid, boundaries, delta_t,
+                                              boundary_conditions_type, information)
+        self.__time_steps = time_steps
+        self.__cells = cells
+        self.__ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
+        self.__fig = plt.figure()
+        self.__delta_t = delta_t
 
-    def add_cell(self, cell):
-        self.add_cells([cell])
+    def __animation_init(self):
+        U = np.cos(X + num * 0.1)
+        V = np.sin(Y + num * 0.1)
 
-    def add_cells(self, cells):
-        self.cells = np.concatenate((self.cells, cells))
+        Q.set_UVC(U, V)
 
-    def remove_cell(self, cell_to_remove):
-        cell_index = np.where(self.cells == cell_to_remove)
-        if len(cell_index) != 0:
-            self.cells = np.delete(self.cells, cell_index[0])
-            return True
-        return False
+        return Q,
 
-    def simulation_start(self):
-        # initial data
-        C.ENV.process(self.main_loop())
-        for cell in self.cells:
-            C.ENV.process(cell.update())
-        C.ENV.run(until=self.time)
 
-    def main_loop(self):
-        self.axes.set_aspect('equal')
-        # make the main loop run after all the cells grow
-        yield C.ENV.timeout(C.DELTA_T / 1000)
-        while True:
-            plt.cla()
-            for cell in self.cells:
-                cell_status = cell.get_status()
-                if cell_status["state"] == Cell.State.FINISHED_SPLITTING:
-                    self.remove_cell(cell)
-                    self.add_cells(cell_status["new_cells"])
-            self.calculate_boundaries()
-            boundaries = self.get_boundaries()
-            for cell_boundary in boundaries:
-                self.axes.add_line(cell_boundary)
+        return self.__domain.quiver()
 
-            plt.pause(0.1)
-            yield C.ENV.timeout(C.DELTA_T)
+    def __animation_main(self, i):
+        self.__domain.next_step()
+        return self.__domain.quiver()
 
-    def calculate_boundaries(self):
-        for cell in self.cells:
-            cell.calculate_boundaries()
-
-    def get_boundaries(self):
-        boundaries = [plb.plot([], [])[0] for _ in range(len(self.cells))]
-        for i, cell in enumerate(self.cells, start=0):
-            cell_boundaries = cell.get_boundaries()
-            boundaries[i].set_data(cell_boundaries[0], cell_boundaries[1])
-        return boundaries
+    def start(self):
+        anim = animation.FuncAnimation(self.__fig, self.__animation_main, init_func=self.__animation_init,
+                                       frames=self.__time_steps, interval=20, blit=True)
+        # anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+        plt.show()
