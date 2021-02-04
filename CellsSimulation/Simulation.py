46d30bc33157d249680f1abbf71575861f4de32d
dor dolev <ddoolev@gmail.com>
2021-02-04 17:08:31 +0200

    modify simulation to "funcanimation" and change lead_driven_cavity accordingly

--- CellsSimulation/Simulation.py
+++ CellsSimulation/Simulation.py
@@ -3,6 +3,9 @@ import GeneralEnums as E
 import numpy as np
 from NavierStokesEquations import NavierStokesEquations
 from matplotlib import animation
+from MathFunctions import interpolate
+import Point
+from Boundaries import Boundaries
 
 
 class Simulation:
@@ -11,31 +14,57 @@ class Simulation:
                  boundary_conditions_type=E.BoundaryConditionsType.neumann,
                  information=E.Information.none,
                  cells=np.array([])):
+        self.__num_of_points = 10
         self.__domain = NavierStokesEquations(fields, grid, boundaries, delta_t,
                                               boundary_conditions_type, information)
+        self.__grid = grid
+        self.__boundaries = boundaries
         self.__time_steps = time_steps
         self.__cells = cells
-        self.__ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
         self.__fig = plt.figure()
+        self.__ax = plt.axes(xlim=(0, 1), ylim=(0, 1))
         self.__delta_t = delta_t
 
+        self.__points = Point.create_random_point_array(self.__num_of_points)
+        array_of_points = self.__points.to_array()
+        self.__points_scatter = self.__ax.scatter(array_of_points[0], array_of_points[1], s=60)
+        self.__lines = [[[], []]]
+        self.__lines2d = self.__ax.plot([], [], lw=1)
+        for i in range(self.__num_of_points - 1):
+            self.__lines.append([[], []])
+            self.__lines2d = np.concatenate((self.__lines2d, self.__ax.plot([], [], lw=1)))
+
     def __animation_init(self):
-        U = np.cos(X + num * 0.1)
-        V = np.sin(Y + num * 0.1)
-
-        Q.set_UVC(U, V)
-
-        return Q,
-
-
-        return self.__domain.quiver()
+        self.__update_lines_and_points()
+        return self.__lines2d
 
     def __animation_main(self, i):
+        print("*********************** time step " + str(i))
         self.__domain.next_step()
-        return self.__domain.quiver()
+        u_field = Boundaries.add_boundaries(self.__domain.fields.u, self.__boundaries, E.Field.u, E.Orientation.all)
+        v_field = Boundaries.add_boundaries(self.__domain.fields.v, self.__boundaries, E.Field.v, E.Orientation.all)
+        grid_delta_y = np.concatenate(([self.__grid.delta_y[0]/2], self.__grid.delta_y, [self.__grid.delta_y[-1]/2]))
+        grid_delta_x = np.concatenate(([self.__grid.delta_x[0]/2], self.__grid.delta_x, [self.__grid.delta_x[-1]/2]))
+        u_field = interpolate(u_field, self.__grid.x, self.__grid.half_y,
+                              self.__grid.delta_half_x, grid_delta_y, self.__points)
+        v_field = interpolate(v_field, self.__grid.half_x, self.__grid.y,
+                              grid_delta_x, self.__grid.delta_half_y, self.__points)
+        self.__points.x = self.__points.x + u_field * self.__delta_t
+        self.__points.y = self.__points.y + v_field * self.__delta_t
+        self.__update_lines_and_points()
+        return self.__lines2d, self.__points.to_array_of_points()
+
+    def __update_lines_and_points(self):
+        array_of_points = self.__points.to_array_of_points()
+        for i in range(self.__num_of_points):
+            self.__lines[i][0].append(array_of_points[i][0])
+            self.__lines[i][1].append(array_of_points[i][1])
+            self.__lines2d[i].set_data(self.__lines[i][0], self.__lines[i][1])
+        array_of_points = self.__points.to_array()
+        self.__points_scatter = self.__ax.scatter(array_of_points[0], array_of_points[1], s=60)
 
     def start(self):
-        anim = animation.FuncAnimation(self.__fig, self.__animation_main, init_func=self.__animation_init,
-                                       frames=self.__time_steps, interval=20, blit=True)
-        # anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
+        self.__anim = animation.FuncAnimation(self.__fig, self.__animation_main, init_func=self.__animation_init,
+                                       frames=self.__time_steps, interval=1, blit=True)
+        self.__anim.save('basic_animation.mp4', fps=30, extra_args=['-vcodec', 'libx264'])
         plt.show()
--- CellsSimulation/tests/lead_driven_cavity.py
+++ CellsSimulation/tests/lead_driven_cavity.py
@@ -1,15 +1,12 @@
+from time import sleep
+
 import numpy as np
 from Boundaries import Boundaries
-from NavierStokesEquations import NavierStokesEquations
-import matplotlib.pyplot as plt
 from GeneralEnums import Field, Orientation, Information
 from Grid import Grid2
 from Fields import Fields2
 import Constants as C
-
-
-def is_in_steady_state():
-    return (delta_u.max() < epsilon) and (delta_v.max() < epsilon) and (delta_p.max() < epsilon)
+from Simulation import Simulation
 
 
 if __name__ == "__main__":
@@ -38,39 +35,7 @@ if __name__ == "__main__":
                              Orientation.top: boundary_top,
                              Orientation.bottom: boundary_bottom})
 
-    domain = NavierStokesEquations(fields_matrix, delta_xy, boundaries, delta_t, information=Information.all)
+    sim = Simulation(fields_matrix, delta_xy, boundaries, delta_t, information=Information.all)
+    sim.start()
+    sleep(1000000)
 
-    new_fields = domain.fields
-    time = 0
-    step_counter = 0
-    epsilon = 0.00001
-    while True:
-        print("time = ", time)
-        old_fields = domain.fields.copy()
-        plt.cla()
-        domain.next_step()
-        delta_u = (new_fields.u - old_fields.u) / new_fields.u
-        delta_v = (new_fields.v - old_fields.v) / new_fields.v
-        tmp_filed_matrix_p = new_fields.p
-        tmp_filed_matrix_p[0][0] = 1
-        delta_p = (new_fields.p - old_fields.p) / tmp_filed_matrix_p
-        print("delta_u max = ", delta_u.max(), "\tdelta_v max = ", delta_v.max(), "\tdelta_p max = ", delta_p.max())
-        if step_counter % 10 == 0:
-            plt.title(("time = ", str(time)))
-            domain.quiver()
-            plt.pause(0.1)
-            if is_in_steady_state():
-                middle_u = new_fields.u.T[int((grid_size + 2) / 2)].T
-                middle_v = new_fields.v[int((grid_size + 2) / 2)]
-                print("middle u = ", middle_u)
-                print("middle v = ", middle_v)
-                print("In steady state")
-                print("Reynold number = ", C.RE)
-                plt.title(("time = ", str(time), "\tReynold number = ", C.RE))
-                domain.quiver()
-                plt.show()
-                break
-        step_counter += 1
-        time += C.DELTA_T
-        print()
-        print()
